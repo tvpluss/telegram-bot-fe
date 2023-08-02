f622d2a65a4538a7d4001abbc09769b5ad70903b
Mateus <phuoctruong.vn@tribox.me>
2023-08-02 14:54:31 +0700

    add auto video player

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,61 @@
 import React, { useEffect, useState } from "react";
-import { Button, Col, Layout, List, Menu, Row } from "antd";
+import {
+  Button,
+  Col,
+  Layout,
+  List,
+  Menu,
+  Row,
+  Skeleton,
+  Typography,
+  message,
+} from "antd";
 import Link from "antd/es/typography/Link";
-
+import YouTube from "react-youtube";
 const { Header, Content } = Layout;
 interface IVideoMetaData {
   name?: string;
-  url?: string;
+  videoId?: string;
   loading?: boolean;
 }
-const youtubeVideos: IVideoMetaData[] = [
+const youtubeVideos: Array<IVideoMetaData> = [
   {
     name: "Tay Trái Chỉ Trăng ",
-    url: "https://www.youtube.com/watch?v=ATPulcGQ2SE",
+    videoId: "ATPulcGQ2SE",
   },
   {
     name: "Ngày ấy",
-    url: "https://www.youtube.com/watch?v=aWNu0BHuHws",
+    videoId: "aWNu0BHuHws",
   },
   {
     name: "You will always...",
-    url: "https://www.youtube.com/watch?v=xpHmDZfg6z4",
+    videoId: "xpHmDZfg6z4",
   },
 ];
-const count = 3;
 
 const App: React.FC = () => {
   const [initLoading, setInitLoading] = useState(true);
   const [loading, setLoading] = useState(false);
-  const [data, setData] = useState<IVideoMetaData[]>([]);
+  const [videoLoading, setVideoLoading] = useState(true);
   const [list, setList] = useState<IVideoMetaData[]>([]);
+  const [curVideo, setCurVideo] = useState<IVideoMetaData>({});
   useEffect(() => {
     setInitLoading(false);
-    setData(youtubeVideos);
-    setList(youtubeVideos);
+    setList(youtubeVideos.slice(1));
+    setCurVideo(youtubeVideos[0]);
+    setVideoLoading(false);
   }, []);
-  const onLoadMore = () => {
+
+  const handleLoadMore = () => {
     setLoading(true);
-    setList(data.concat([...new Array(count)].map(() => ({}))));
-    const newData = data.concat(youtubeVideos);
-    setData(newData);
-    setList(newData);
+    setList((prevList) => [...prevList, ...youtubeVideos]);
     setLoading(false);
+    // return false
     // Resetting window's offsetTop so as to display react-virtualized demo underfloor.
     // In real scene, you can using public method of react-virtualized:
     // https://stackoverflow.com/questions/46700726/how-to-use-public-method-updateposition-of-react-virtualized
     window.dispatchEvent(new Event("resize"));
+    return true;
   };
   const loadMore =
     !initLoading && !loading ? (
@@ -56,9 +67,39 @@ const App: React.FC = () => {
           lineHeight: "32px",
         }}
       >
-        <Button onClick={onLoadMore}>loading more</Button>
+        <Button onClick={handleLoadMore}>loading more</Button>
       </div>
     ) : null;
+  const changeVideo = async () => {
+    try {
+      if (list.length == 0) {
+        message.info(`Đã hết video trong queue`);
+        return;
+      }
+      setVideoLoading(true);
+      setCurVideo(list[0]);
+      setList(list.slice(1));
+      setVideoLoading(false);
+      message.info(`Kết thúc video`);
+    } catch (error) {
+      message.error(`Lỗi khi chuyển video:`);
+    }
+  };
+  const handleVideoError = async (error: any) => {
+    message.error(`Lỗi của video: ${error}`);
+    await changeVideo();
+  };
+  const handleVideoEnded = async () => {
+    await changeVideo();
+  };
+  const opts = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
   return (
     <Layout>
       <Header
@@ -71,16 +112,9 @@ const App: React.FC = () => {
           alignItems: "center",
         }}
       >
-        <div className="demo-logo" />
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={["2"]}
-          items={new Array(3).fill(null).map((_, index) => ({
-            key: String(index + 1),
-            label: `nav ${index + 1}`,
-          }))}
-        />
+        <Typography.Title type="success" level={1}>
+          Auto Videos Player
+        </Typography.Title>
       </Header>
       <Content
         className="site-layout"
@@ -88,18 +122,18 @@ const App: React.FC = () => {
       >
         <Row gutter={10}>
           <Col flex={2}>
-            <iframe
-              src="https://www.youtube.com/embed/E7wJTI-1dvQ"
-              allow="autoplay; encrypted-media"
-              title="video"
-              allowFullScreen
-              style={{
-                minWidth: "800",
-                minHeight: "600",
-              }}
-            />
+            <Typography.Title level={2}>Current Video</Typography.Title>
+            <Skeleton active loading={videoLoading}>
+              <YouTube
+                videoId={curVideo.videoId}
+                opts={opts}
+                onEnd={handleVideoEnded}
+                onError={handleVideoError}
+              />
+            </Skeleton>
           </Col>
           <Col flex={1}>
+            <Typography.Title level={2}>Requested</Typography.Title>
             <List
               className="demo-loadmore-list"
               loading={initLoading}
@@ -115,7 +149,7 @@ const App: React.FC = () => {
                 >
                   <List.Item.Meta
                     title={<a href="https://ant.design">{item.name}</a>}
-                    description={<Link>{item.url}</Link>}
+                    description={<Link>{item.videoId}</Link>}
                   />
                 </List.Item>
               )}

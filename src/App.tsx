7e131ba4df4b15689fb709747a7e82c38777e5d8
Mateus <phuoctruong.vn@tribox.me>
2023-08-02 12:04:40 +0700

    add basic list

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,130 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import { Avatar, Button, Col, Layout, List, Menu, Row, Skeleton } from "antd";
+import Link from "antd/es/typography/Link";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const { Header, Content } = Layout;
+interface IVideoMetaData {
+  name?: string;
+  url?: string;
+  loading?: boolean;
 }
+const youtubeVideos: IVideoMetaData[] = [
+  {
+    name: "Tay Trái Chỉ Trăng ",
+    url: "https://www.youtube.com/watch?v=ATPulcGQ2SE",
+  },
+  {
+    name: "Ngày ấy",
+    url: "https://www.youtube.com/watch?v=aWNu0BHuHws",
+  },
+  {
+    name: "You will always...",
+    url: "https://www.youtube.com/watch?v=xpHmDZfg6z4",
+  },
+];
+const count = 3;
 
-export default App
+const App: React.FC = () => {
+  const [initLoading, setInitLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState<IVideoMetaData[]>([]);
+  const [list, setList] = useState<IVideoMetaData[]>([]);
+  useEffect(() => {
+    setInitLoading(false);
+    setData(youtubeVideos);
+    setList(youtubeVideos);
+  }, []);
+  const onLoadMore = () => {
+    setLoading(true);
+    setList(data.concat([...new Array(count)].map(() => ({}))));
+    const newData = data.concat(youtubeVideos);
+    setData(newData);
+    setList(newData);
+    setLoading(false);
+    // Resetting window's offsetTop so as to display react-virtualized demo underfloor.
+    // In real scene, you can using public method of react-virtualized:
+    // https://stackoverflow.com/questions/46700726/how-to-use-public-method-updateposition-of-react-virtualized
+    window.dispatchEvent(new Event("resize"));
+  };
+  const loadMore =
+    !initLoading && !loading ? (
+      <div
+        style={{
+          textAlign: "center",
+          marginTop: 12,
+          height: 32,
+          lineHeight: "32px",
+        }}
+      >
+        <Button onClick={onLoadMore}>loading more</Button>
+      </div>
+    ) : null;
+  return (
+    <Layout>
+      <Header
+        style={{
+          position: "sticky",
+          top: 0,
+          zIndex: 1,
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <div className="demo-logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["2"]}
+          items={new Array(3).fill(null).map((_, index) => ({
+            key: String(index + 1),
+            label: `nav ${index + 1}`,
+          }))}
+        />
+      </Header>
+      <Content
+        className="site-layout"
+        style={{ padding: "0 20px", minHeight: "100vh" }}
+      >
+        <Row gutter={10}>
+          <Col flex={2}>
+            <iframe
+              src="https://www.youtube.com/embed/E7wJTI-1dvQ"
+              allow="autoplay; encrypted-media"
+              title="video"
+              allowFullScreen
+              style={{
+                minWidth: "800",
+                minHeight: "600",
+              }}
+            />
+          </Col>
+          <Col flex={1}>
+            <List
+              className="demo-loadmore-list"
+              loading={initLoading}
+              itemLayout="horizontal"
+              loadMore={loadMore}
+              dataSource={list}
+              renderItem={(item) => (
+                <List.Item
+                  actions={[
+                    <a key="list-loadmore-edit">edit</a>,
+                    <a key="list-loadmore-more">more</a>,
+                  ]}
+                >
+                  <List.Item.Meta
+                    title={<a href="https://ant.design">{item.name}</a>}
+                    description={<Link>{item.url}</Link>}
+                  />
+                </List.Item>
+              )}
+            />
+          </Col>
+        </Row>
+      </Content>
+    </Layout>
+  );
+};
+
+export default App;
